f1ffff77011144031f6835a76f6016f4eaac7d6e
Alfredo Revilla <alfredo.revilla@whoiam.ai>
2020-05-25 21:35:40 -0500

    sql package change. logger added.

--- Controllers/SqlController.cs
+++ Controllers/SqlController.cs
@@ -1,8 +1,12 @@
-﻿using System.Data.SqlClient;
+﻿using Microsoft.Data.SqlClient;
+using System.Data.SqlClient;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Services.AppAuthentication;
 using Microsoft.Extensions.Configuration;
+using System.Diagnostics;
+using System;
+using Microsoft.Extensions.Logging;
 
 namespace webapi_azure_ad_managed_identity_with_sql.Controllers
 {
@@ -11,17 +15,19 @@ namespace webapi_azure_ad_managed_identity_with_sql.Controllers
     public class SqlController : ControllerBase
     {
         private readonly IConfiguration configuration;
+        private readonly ILogger<SqlController> logger;
 
-        public SqlController(IConfiguration configuration)
+        public SqlController(IConfiguration configuration, ILogger<SqlController> logger)
         {
             this.configuration = configuration;
+            this.logger = logger;
         }
 
         [HttpGet("version")]
         public async Task<IActionResult> GetVersionAsync()
         {
             var azureServiceTokenProvider = new AzureServiceTokenProvider();
-            var accessToken = await azureServiceTokenProvider.GetAccessTokenAsync("https://database.windows.net");
+            var accessToken = await azureServiceTokenProvider.GetAccessTokenAsync("https://database.windows.net/");
 
             using var sqlConnection = new SqlConnection(configuration.GetConnectionString("Default"))
             {
